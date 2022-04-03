0103a44ab42ae50dc1b32cfd5bc2d89cb3e40599
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2022-04-03 16:08:54 +0700

    backend commit#1

--- django/core/settings.py
+++ django/core/settings.py
@@ -126,3 +126,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"]
+}
